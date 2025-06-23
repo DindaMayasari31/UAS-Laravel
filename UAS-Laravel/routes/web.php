9af4d2d00c582959caf84d443f4a41478bd7a28f
DindaMayasari31 <dindamayasari31@gmail.com>
2025-06-23 20:06:56 +0700

    mengirimkan data ke view

--- UAS-Laravel/routes/web.php
+++ UAS-Laravel/routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name"=>"Dinda Mayasari",
+        "email"=>"dindamayasari31@gmail.com",
+        "image"=>"riwayat.jpg"
+    ]
+);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
