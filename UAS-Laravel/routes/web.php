eeff7784503b19ad47cd8e9b6bd49910562dbbe6
DindaMayasari31 <dindamayasari31@gmail.com>
2025-06-25 16:05:31 +0700

    layoting system

--- UAS-Laravel/routes/web.php
+++ UAS-Laravel/routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title"=>"Home"
+    ]);
+    
 });
 
 Route::get('/about', function () {
     return view('about',[
+        "title"=>"about",
         "name"=>"Dinda Mayasari",
         "email"=>"dindamayasari31@gmail.com",
         "image"=>"riwayat.jpg",
@@ -16,5 +20,8 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts',[
+        "title"=>"Posts"
+    ]);
+
 });
