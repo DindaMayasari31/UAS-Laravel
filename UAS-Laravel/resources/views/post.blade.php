aec6ba03aff3be12d85da4e5bef2fc635b7eda8c
DindaMayasari31 <dindamayasari31@gmail.com>
2025-07-03 16:02:12 +0700

    single post

--- UAS-Laravel/resources/views/post.blade.php
+++ UAS-Laravel/resources/views/post.blade.php
@@ -1,12 +1,13 @@
+
+
 @extends('layouts.main')
 
 
 @section('container')
 <article>
-<h2>Judul</h2>
-<h5>Author</h5>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab libero amet dicta quis, nulla harum deserunt provident necessitatibus magni autem illo? Maiores obcaecati et,
- dolorem officia vitae sapiente ipsum voluptatibus.</p>
+<h2>{{ $post["title"] }}</h2>
+<h5>{{ $post["author"] }}</h5>
+<p>{{ $post["body"]}}</p>
  </article>
 
  <a href="/blog">Back to Posts</a>
